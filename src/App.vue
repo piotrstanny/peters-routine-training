25d4f5dd71a1a3927fc0e0e175a4412f2949e691
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-14 20:44:45 +0100

    Create Header component and add logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,13 +9,31 @@
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style lang="scss">
+$bg-color: #1e2124;
+
+body {
+  background-color: $bg-color;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f3f3f3;
+
 }
 
 #nav {
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header">
+    <img src="@/assets/logo.png" alt="Peter's Routine logo">
+  </div>
+</template>
+
+<style scoped lang="scss">
+  #header {
+    width: 100%;
+    max-height: 200px;
+    background-color: black;
+
+    img {
+      max-height: 200px;
+    }
+  }
+</style>
