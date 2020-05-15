6b824f30721901066673c3ba30c9d424164d97cf
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-15 22:51:32 +0100

    Implement cards in home options

--- src/components/HomeOptions.vue
+++ src/components/HomeOptions.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    <p>some options list here</p>
+  <div class="options">
+    <div class="row row-cols-2 justify-content-center">
+
+      <div class="col-xs-5">
+        <div class="card bg-info">
+          <div class="card-body">
+            <h5 class="card-title">Timers</h5>
+            <a href="#" class="btn btn-primary">Choose</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-5">
+        <div class="card bg-dark">
+          <div class="card-body">
+            <h5 class="card-title">Profile</h5>
+            <a href="#" class="btn btn-primary">Choose</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-5">
+        <div class="card bg-info">
+          <div class="card-body">
+            <h5 class="card-title">Timers</h5>
+            <a href="#" class="btn btn-primary">Choose</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-5">
+        <div class="card bg-dark">
+          <div class="card-body">
+            <h5 class="card-title">Profile</h5>
+            <a href="#" class="btn btn-primary">Choose</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+// .card-body {
+//   background-color: rgb(43, 43, 43);
+// }
+.options {
+  padding-top: 50px;
+}
+</style>
