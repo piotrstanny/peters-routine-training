6ce8c405679bd3af8c86a7ef42d8b609ac16fcc5
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-15 21:39:30 +0100

    Clear Home router page

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ body {
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #c0c0c0;
 
     &.router-link-exact-active {
       color: #42b983;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ul>
+      <li>
+        List of tools
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
 </script>
+
+<style lang="scss" scoped>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+</style>
