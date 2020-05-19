e490f1b85dedb28e4b417abc2da6054e641ebcac
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-19 21:39:32 +0100

    Add form structure to a Single Timer

--- src/views/timers/SingleTimer.vue
+++ src/views/timers/SingleTimer.vue
@@ -2,5 +2,49 @@
   <div>
     <h3>Single timer {{ $route.params.id }}</h3><br>
     <p>Form of intervals to be created</p>
+    <form>
+      <div class="form-group row m-4">
+        <label
+        for="colFormLabelSm"
+        class="col-sm-2 col-form-label col-form-label-sm">
+          Set Time:
+        </label>
+        <div class="col-sm-10">
+          <input
+          type="email"
+          class="form-control form-control-sm"
+          id="colFormLabelSm"
+          placeholder="col-form-label-sm">
+        </div>
+      </div>
+      <div class="form-group row m-4">
+        <label
+        for="colFormLabelSm"
+        class="col-sm-2 col-form-label col-form-label-sm">
+          Set Time:
+        </label>
+        <div class="col-sm-10">
+          <input
+          type="email"
+          class="form-control form-control-sm"
+          id="colFormLabelSm"
+          placeholder="col-form-label-sm">
+        </div>
+      </div>
+      <div class="form-group row m-4">
+        <label
+        for="colFormLabelSm"
+        class="col-sm-2 col-form-label col-form-label-sm">
+          Set Time:
+        </label>
+        <div class="col-sm-10">
+          <input
+          type="email"
+          class="form-control form-control-sm"
+          id="colFormLabelSm"
+          placeholder="col-form-label-sm">
+        </div>
+      </div>
+    </form>
   </div>
 </template>
