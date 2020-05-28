ec20430b5771e211aabb22a4219d10c497c01657
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-28 15:26:09 +0100

    Import font awesome icons in main.js and create component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPlay,
+  faRedoAlt,
+  faBackspace,
+  faStepForward,
+  faStepBackward,
+  faMinusSquare,
+  faPlusSquare,
+  faHome,
+  faStopwatch,
+  faUserAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+
+library.add(
+  faPlay,
+  faRedoAlt,
+  faBackspace,
+  faStepForward,
+  faStepBackward,
+  faMinusSquare,
+  faPlusSquare,
+  faHome,
+  faStopwatch,
+  faUserAlt,
+);
+
+Vue.component('fa-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
