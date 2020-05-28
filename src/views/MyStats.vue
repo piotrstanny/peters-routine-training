5adb8bf6d49a9ced32409262a3e4111456d05cad
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-28 15:55:51 +0100

    Change text of my stats page

--- src/views/MyStats.vue
+++ src/views/MyStats.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mt-5">
-    <h3>Your profile metrics</h3>
+    <h3>Your profile</h3>
     <p style="font-size: 1.3em">
-      Functionality of your performance analytics is going to be built here
+      Page under construction.
     </p>
     <ul>
       <li v-for="(item, index) in 5" :key="index" style="text-align: left;">
