6ffb414961b5eb75c5d15a2644404783ac3c663a
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-15 12:19:36 +0100

    Create EventCard to display it on the list of event as reusable components

---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mt-5">
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Bartlett Cross Training',
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
