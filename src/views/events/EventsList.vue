02577dbb9303e110068617e0cd247af48fd19ad1
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-16 22:51:52 +0100

    Add axios and load json data into events list component

--- src/views/events/EventsList.vue
+++ src/views/events/EventsList.vue
@@ -16,10 +16,21 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
 
 export default {
   components: {
     EventCard,
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(`There was an error: ${error}`);
+      });
   },
 };
 </script>
