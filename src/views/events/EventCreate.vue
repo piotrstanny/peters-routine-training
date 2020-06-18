af5a0b9ff6b8eff3eea8a08f51b7b48d620b51b9
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-18 13:27:09 +0100

    Setp up Vuex store file; display state details

--- src/views/events/EventCreate.vue
+++ src/views/events/EventCreate.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="container mt-5">
     <h1>Create New Event</h1>
+    <p>Hello {{ userName }}!</p>
+    <h4>Event's details:</h4>
+    <p>Creator: {{ userName }}</p>
+    <p>Creator's ID: {{ userId }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name;
+    },
+    userId() {
+      return this.$store.state.user.id;
+    },
+  },
+};
+</script>
