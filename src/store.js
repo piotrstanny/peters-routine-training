cb24446d41397de889df87d5733438d9e7a2bbad
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-19 15:56:43 +0100

    Move fetching events from component to the store

--- src/store.js
+++ src/store.js
@@ -22,12 +22,24 @@ export default new Vuex.Store({
     ADD_EVENT(state, event) {
       state.events.push(event);
     },
+    SET_EVENTS(state, events) {
+      state.events = events;
+    },
   },
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event);
       });
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then((response) => {
+          commit('SET_EVENTS', response.data);
+        })
+        .catch((error) => {
+          console.log(`There was an error: ${error}`);
+        });
     },
   },
   getters: {
