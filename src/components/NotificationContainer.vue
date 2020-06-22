081bf7c421fe8b504b6ac68a4b424b92eda3fa36
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-22 17:37:55 +0100

    Add notification container logic fetching messages from vuex

---
+++ src/components/NotificationContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mt-5">
+    <NotificationBar
+    v-for="notification in notifications"
+    :key="notification.id"
+    :notification="notification"/>
+  </div>
+</template>
+
+<script>
+import NotificationBar from '../components/NotificationBar';
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+    NotificationBar,
+  },
+  computed: mapState(notifications, ['notifications']),
+}
+</script>
