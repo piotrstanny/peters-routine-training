1b18bd9a0a6ec7c6bf1d7cea0d90f0a97d390d10
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-22 17:20:46 +0100

    Add notification components; Add notification bar logic

---
+++ src/components/NotificationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-1" :class="notificationTypeClass">
+    <p>{{ notification.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    notification: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    notificationTypeClass() {
+      return `-text-${this.notification.type}`;
+    },
+  },
+};
+</script>
+
+<style>
+.-text-error {
+  color: red;
+}
+.-text-success {
+  color: green;
+}
+</style>
