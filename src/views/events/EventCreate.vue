150c77f10e71835ba04c777137e0739f3942a952
Piotr Stanny <piotr.stanny@gmail.com>
2020-06-24 00:08:47 +0100

    Style create event form with bootstrap

--- src/views/events/EventCreate.vue
+++ src/views/events/EventCreate.vue
@@ -1,41 +1,94 @@
 <template>
   <div class="container mt-5">
-    <h1>Create New Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
-      <h3>Name and describe your event</h3>
-      <div class="field">
-        <label>Organizer: </label>
-        <span> {{ event.user.name }}</span>
+
+      <div class="card bg-dark m-1 mt-2">
+        <div class="card-header font-weight-bold">
+          Create New Event
+        </div>
+        <div class="card-body pt-1">
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm">
+              Title:
+            </label>
+            <div class="row m-0">
+              <input v-model="event.title" type="text" placeholder="Add an event title"/>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm"
+            >Select a category:</label>
+            <div class="row m-0">
+            <select v-model="event.category">
+              <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+            </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm">
+              Description:
+            </label>
+            <div class="row m-0">
+              <textarea
+              cols="40"
+              v-model="event.description"
+              type="text"
+              placeholder="Add a description..."/>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm">
+              Location:
+            </label>
+            <div class="row m-0">
+              <input v-model="event.location" type="text" placeholder="Add a location"/>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm">
+              Date:
+            </label>
+            <div class="row m-0">
+              <Datepicker v-model="event.date" placeholder="Select a date"></Datepicker>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label
+            for="colFormLabelSm"
+            class="col-form-label col-form-label-sm">
+              Select a Time:
+            </label>
+            <div class="row m-0">
+              <select v-model="event.time">
+                <option v-for="time in times" :key="time">{{ time }}</option>
+              </select>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="btn btn-block btn-info">
+            <strong>Create Event</strong>
+          </button>
+
+        </div>
       </div>
-      <div class="field">
-        <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
-      </div>
-      <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
-      </div>
-      <h3>When is your event?</h3>
-      <div class="field">
-        <label>Date</label>
-        <Datepicker v-model="event.date" placeholder="Select a date"></Datepicker>
-      </div>
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
-      <input type="submit" class="button -fill-gradient" value="Submit"/>
     </form>
   </div>
 </template>
@@ -49,7 +102,7 @@ export default {
   },
   data() {
     const times = [];
-    for (let i = 1; i <= 24; i += 1) {
+    for (let i = 9; i <= 24; i += 1) {
       times.push(`${i}:00`);
     }
     return {
